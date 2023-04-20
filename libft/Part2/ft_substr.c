0c137724e2253a30a5074d1172b509c31042535d
Caio Rocha Sobreira <crocha-s@c1r6s7.42porto.com>
2023-04-20 16:55:44 +0100

    Part2

---
+++ libft/Part2/ft_substr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: crocha-s <crocha-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/19 19:02:03 by crocha-s          #+#    #+#             */
+/*   Updated: 2023/04/19 21:19:41 by crocha-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char			*substring;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	substring = (char *)malloc(((len) * sizeof (char)) + 1);
+	if (substring == NULL)
+	{
+		return (NULL);
+	}
+	while (s[i] != '\0')
+	{
+		if( (i >= start && j < len))
+		{
+			substring[j] = s[i];
+			j++;
+		}
+		i++;
+	}
+	substring[j] = '\0';
+	return (substring);
+}
